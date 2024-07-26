4413559bb52905928a108dd4c4feb0bf53013e8c
seojaehong1 <wtme3@naver.com>
2024-07-26 14:58:01 +0900

    Add serializers, update views, and create new models

--- production/models.py
+++ production/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    expiration_date = models.DateField()
+    category = models.CharField(max_length=100, blank=True, null=True)
+    location = models.CharField(max_length=100, choices=[('냉장', '냉장'), ('냉동', '냉동'), ('실외', '실외')], blank=True, null=True)
+    quantity = models.IntegerField(blank=True, null=True)
+    memo = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
