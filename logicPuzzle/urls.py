d64ac7a2daf245b71d349efea814817aad1ab732
byj <yeongjun1324@gmail.com>
2024-09-05 22:50:52 +0900

    fix : swagger setting

--- logicPuzzle/urls.py
+++ logicPuzzle/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework.permissions import AllowAny
+from rest_framework import permissions, routers
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -30,10 +29,11 @@ schema_view = get_schema_view(
         license=openapi.License(name="BSD License"),
     ),
     public=True,
-    permission_classes=[AllowAny],
+    permission_classes=[permissions.AllowAny],
 )
 
 urlpatterns = [
+    path('api/v1/user/', include('user.urls', namespace='user')),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
