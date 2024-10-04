81bfd6341574ea28399e71dc5e23ca921419e5b1
seojaehong1 <wtme3@naver.com>
2024-10-04 09:04:45 +0900

    Add untracked files

---
+++ db_settings.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'LOGIC_PUZZLE',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': '13.125.161.62',
+        'PORT': '3306',
+    }
+}
