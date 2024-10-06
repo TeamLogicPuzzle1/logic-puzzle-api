f3f07d4aaf9b8d04c7fc7f30a7bcb65d851258d2
yjb <yeognjun1324@gmail.com>
2024-10-06 22:31:05 +0900

    feating : member api

--- profile/models.py
+++ profile/models.py
@@ -6,7 +6,7 @@ import user.models
 # Create your models here.
 class Profile(models.Model):
     id = models.AutoField(primary_key=True)
-    user_id = models.ForeignKey(user.models.User, max_length=20, on_delete=models.PROTECT)
+    user = models.ForeignKey(user.models.User, max_length=20, on_delete=models.PROTECT)
     profile_name = models.CharField(max_length=20)
     pin_num = models.CharField(max_length=128, null=False, blank=False)
     leader_yn = models.BooleanField(default=False)
---
+++ profile/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from .views import CreateProfile
+
+app_name = 'user'
+
+urlpatterns = [
+]
