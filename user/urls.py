3ca9a60c5e67a476e0559eb2a15e092d3a03f18b
yjb <yeognjun1324@gmail.com>
2024-11-10 00:31:45 +0900

    fix : fix to urlpatterns

--- user/urls.py
+++ user/urls.py
@@ -6,7 +6,7 @@ app_name = 'user'
 
 urlpatterns = [
     path('signup', SignupAPIView.as_view(), name='signup'),
-    path('check', UserCheckAPIView.as_view(), name='check'),
-    path('sendVerifyCode', SendVerifyCode.as_view(), name='sendVerifyCode'),
-    path('checkVerifyCode', CheckVerifyCode.as_view(), name='checkVerifyCode')
+    path('checkId', UserCheckAPIView.as_view(), name='checkId'),
+    path('send-verify-code', SendVerifyCode.as_view(), name='sendVerifyCode'),
+    path('check-verify-code', CheckVerifyCode.as_view(), name='checkVerifyCode')
 ]
