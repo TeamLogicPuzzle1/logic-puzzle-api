2c31f367ab2f1fbedaace74c0be3bd6a290ad011
seojaehong1 <wtme3@naver.com>
2024-11-13 17:21:38 +0900

    Add new migration for product_id

--- notification/urls.py
+++
@@ -1,7 +0,0 @@
-# urls.py
-from django.urls import path
-from .views import send_notification_view  # 알맞은 경로로 임포트
-
-urlpatterns = [
-    path('send-notification/', send_notification_view, name='send_notification'),
-]
