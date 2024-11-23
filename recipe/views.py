d473d6d6778b88ff300765f378de2c36450e7b6e
seojaehong1 <wtme3@naver.com>
2024-11-23 19:11:27 +0900

    update recipe/service.py, recipe/views.py

--- recipe/views.py
+++ recipe/views.py
@@ -5,6 +5,7 @@ from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework import permissions
 from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from recipe.service import RecipeService
 
@@ -33,6 +34,27 @@ class RecipeListAPIView(APIView):
     def get(self, request):
         prod_names = request.query_params.get('prodNames', "")
         prod_names_list = prod_names.split(',')
-        response = RecipeService.recipeList(prod_names_list)
-        return response
+
+        try:
+            # RecipeService에서 음식과 재료 정보를 함께 조회
+            response_data = RecipeService.recipeList(prod_names_list)
+
+            if not response_data:
+                return Response({'message': '레시피 데이터 없음'}, status=204)
+
+            return Response({'recipes': response_data}, status=200)
+
+
+        except DatabaseError:
+
+            logger.error("레시피 데이터를 가져오는 중 오류 발생")
+
+            return Response({'message': '레시피 데이터를 찾을 수 없습니다.'}, status=500)
+
+
+        except Exception as e:
+
+            logger.error(f"레시피 조회 중 서버 오류 발생: {e}")
+
+            return Response({'message': '서버 오류가 발생했습니다.'}, status=500)
 
